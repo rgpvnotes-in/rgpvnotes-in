754e67b1073d9239d377938569459743e2bd00b9
Aditya Jain - www.aadityajain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2023-03-22 04:28:14 +0530

    added Vue 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [vue()]
+});
