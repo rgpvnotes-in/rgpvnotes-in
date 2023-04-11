cfde020c579ae4f0cda4b739bae6815a52f5578c
Aditya Jain - www.aadityajain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2023-04-11 06:25:46 +0530

    adding prettier and pre-commit hooks

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,20 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es2021: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/vue3-essential",
-        "plugin:@typescript-eslint/recommended"
+    extends: [
+        'eslint:recommended',
+        'plugin:vue/vue3-essential',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
     ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    overrides: [],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
     },
-    "plugins": [
-        "vue",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+    plugins: ['vue', '@typescript-eslint'],
+    rules: {},
+};
