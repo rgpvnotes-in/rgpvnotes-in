26e0ac8e7a246ad04aeac48f8e833c2447191adf
Aditya Jain - www.aadityajain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2023-05-09 04:24:02 +0530

    adding breadcrumbs & inner page component

---
+++ src/components/breadcrumbsComponent/breadcrumbsComponent.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+    <section class="breadcrumbs">
+        <div class="container">
+            <div class="d-flex justify-content-between align-items-center">
+                <h2>Inner Page</h2>
+                <ol>
+                    <li><a href="index.html">Home</a></li>
+                    <li>Inner Page</li>
+                </ol>
+            </div>
+        </div>
+    </section>
+</template>
---
+++ src/components/innerPageComponent/innerPageComponent.vue
@@ -0,0 +1,15 @@
+<script setup>
+import breadcrumbsComponent from '../breadcrumbsComponent/breadcrumbsComponent.vue';
+</script>
+
+<template>
+    <main id="main">
+        <breadcrumbsComponent />
+
+        <section class="inner-page">
+            <div class="container">
+                <p>Example inner page template</p>
+            </div>
+        </section>
+    </main>
+</template>
