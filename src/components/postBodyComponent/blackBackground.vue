eef0aaaf308cbd374e64e4c24a0addfb8e56fb56
Aditya Jain - www.aadityajain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2023-05-11 21:39:52 +0530

    black post-body-title-card

---
+++ src/components/postBodyComponent/blackBackground.vue
@@ -0,0 +1,19 @@
+<script>
+import { computed } from 'vue';
+
+export default {
+  setup(_, { slots }) {
+    const hasHeaderSlot = computed(() => slots.header && slots.header());
+
+    return {
+      hasHeaderSlot,
+    };
+  },
+};
+</script>
+
+<template>
+    <div class="post-body-title-card" v-if="hasHeaderSlot">
+        <slot />
+    </div>
+</template>
