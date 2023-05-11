8c974fedc8acf598fa4969f40452cafcd8fd1182
Aditya Jain - www.aadityajain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2023-05-12 02:25:25 +0530

    adding props in breadCrumbsArray components

--- structure_generator/generateSlugFolderContent.js
+++ structure_generator/generateSlugFolderContent.js
@@ -8,6 +8,7 @@ export const programSchemeSlugContent = (programName = '', schemeName = '') => {
     return `---
 import Layout from "../../../layouts/Layout.astro";
 import InnerPageContainer from '../../../components/innerPageComponent/innerPageComponent.vue';
+import SubjectCard from '../../../components/postBodyComponent/multiColorBackground.vue';
 
 // constants
 const programName = '${programName}';
@@ -15,8 +16,9 @@ const schemeName = '${schemeName}';
 export async function getStaticPaths() {
   const getSlugsData = await fetch(
     'http://backend.rgpvnotes.in/api/v1/all_post.php?program_name=${programName}&scheme_name=${schemeName}'
-  ).then((response) => response.json()).catch((error)=> {console.error(error});
-  return getSlugsData.content.map((post) => {   
+  ).then((response) => response.json()).catch((error)=> {console.error(error)});
+
+  return getSlugsData.content.map((post: any) => {   
     return {
       params: { slug: post.slug },
       props: { post },
@@ -25,11 +27,30 @@ export async function getStaticPaths() {
 }
 const { slug } = Astro.params;
 const { post } = Astro.props;
+
+const pathArray = Astro.url.pathname.split('/');
+const breadCrumbsArray = [];
+
+for (let index = pathArray.length - 1; index >= 0; index--) {
+    let name = '';
+    if (0 === index) {
+        name = 'Home';
+    } else if (1 === index) {
+        name = programName;
+    } else if (2 === index) {
+        name = schemeName;
+    }
+
+    breadCrumbsArray.push({ name: name, url: pathArray.join('/') });
+    pathArray.pop();
+  }
+
 ---
 <Layout title="" isInnerPage={true}>
-  <h1>{slug}</h1>
-  <h1>{JSON.stringify(post.post_id)}</h1>
-  <></>
+<InnerPageContainer staticTitle={post.rendered_title} breadCrumbsArray={breadCrumbsArray.reverse()}>
+<h1>{slug}</h1>
+<h1>{JSON.stringify(post)}</h1>
+</InnerPageContainer>
 </Layout>
 <style></style>`;
 };
