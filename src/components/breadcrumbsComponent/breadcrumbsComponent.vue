c6125cf032b805b2541d3b3e6b6a8cb5c059a47b
Aditya Jain - www.aadityajain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2023-05-11 23:50:44 +0530

    making breadcrumbs component dynamic

--- src/components/breadcrumbsComponent/breadcrumbsComponent.vue
+++ src/components/breadcrumbsComponent/breadcrumbsComponent.vue
@@ -1,13 +1,29 @@
-<script setup></script>
+<script setup>
+const props = defineProps({
+    staticTitle: {
+        required: true,
+        type: String,
+        default: '',
+    },
+    breadCrumbsArray: {
+        required: true,
+        type: Array,
+    },
+});
+</script>
 
 <template>
-    <section class="breadcrumbs">
+    <section class="breadcrumbs" v-if="props.staticTitle">
         <div class="container">
             <div class="d-flex justify-content-between align-items-center">
-                <h2>Inner Page</h2>
+                <h2>{{ props.staticTitle }}</h2>
                 <ol>
-                    <li><a href="index.html">Home</a></li>
-                    <li>Inner Page</li>
+                    <li
+                        v-for="breadCrumb in props.breadCrumbsArray"
+                        :key="breadCrumb.name"
+                    >
+                        <a :href="breadCrumb.url">{{ breadCrumb.name }}</a>
+                    </li>
                 </ol>
             </div>
         </div>
