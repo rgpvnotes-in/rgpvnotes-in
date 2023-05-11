006a7169befbb55f87ce0bdb0be2f9e82ef641da
Aditya Jain - www.aadityajain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2023-05-11 23:55:07 +0530

    using defineProps in component

--- src/components/postBodyComponent/multiColorBackground.vue
+++ src/components/postBodyComponent/multiColorBackground.vue
@@ -1,34 +1,42 @@
-<script>
+<script setup>
 import '@assets/css/blogger-style.css';
+import StaticTitleCard from '../postBodyComponent/blackBackground.vue';
 
-
-export default {
-    props: {
-        getBranchYearDataArray: {
-            required: true,
-            type: Array,
-        },
+const props = defineProps({
+    getBranchYearDataArray: {
+        required: true,
+        type: Array,
     },
-    setup(props) {
-        // eslint-disable-next-line vue/no-setup-props-destructure
-        const allSubjectsData = props.getBranchYearDataArray;
+});
 
-        return {
-            allSubjectsData,
-        };
-    },
-};
+const semesterArray = [];
+for (const subjectsData of props.allSubjectsData) {
+    semesterArray.push(subjectsData.semester);
+}
+
+let filteredSemesterArray = [...new Set(semesterArray)].sort();
+
+const filteredSubjectsData = (semesterName) =>
+    (props.allSubjectsData.filter(
+        (subjectsData) => subjectsData.semester === semesterName,
+    )).sort();
+
 </script>
 
 <template>
-    <div
-        class="custom-fancy-container"
-        :class="'fancy-container-background-' + (index + 1)"
-        :key="subjectObject.post_id"
-        v-for="(subjectObject, index) in allSubjectsData"
-    >
-        <a :href="subjectObject.slug"
-            ><b>{{ subjectObject.rendered_title }}</b></a
+    <div v-for="semesterData in filteredSemesterArray" :key="semesterData">
+        <StaticTitleCard>
+            <h2>{{ semesterData }}</h2>
+        </StaticTitleCard>
+        <div
+            class="custom-fancy-container"
+            :class="'fancy-container-background-' + (index + 1)"
+            :key="subjectObject.post_id"
+            v-for="(subjectObject, index) in filteredSubjectsData(semesterData)"
         >
+            <a :href="subjectObject.slug"
+                ><b>{{ subjectObject.rendered_title }}</b></a
+            >
+        </div>
     </div>
 </template>
