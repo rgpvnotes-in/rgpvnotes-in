1b691470c79c09de2070544d74d184ca8b2e22a0
Aditya Jain - www.aadityajain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2023-05-11 21:41:09 +0530

    multi color card component

---
+++ src/components/postBodyComponent/multiColorBackground.vue
@@ -0,0 +1,34 @@
+<script>
+import '@assets/css/blogger-style.css';
+
+
+export default {
+    props: {
+        getBranchYearDataArray: {
+            required: true,
+            type: Array,
+        },
+    },
+    setup(props) {
+        // eslint-disable-next-line vue/no-setup-props-destructure
+        const allSubjectsData = props.getBranchYearDataArray;
+
+        return {
+            allSubjectsData,
+        };
+    },
+};
+</script>
+
+<template>
+    <div
+        class="custom-fancy-container"
+        :class="'fancy-container-background-' + (index + 1)"
+        :key="subjectObject.post_id"
+        v-for="(subjectObject, index) in allSubjectsData"
+    >
+        <a :href="subjectObject.slug"
+            ><b>{{ subjectObject.rendered_title }}</b></a
+        >
+    </div>
+</template>
