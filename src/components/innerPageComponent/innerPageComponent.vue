a4690d90e34785db4c2c74dc8999f2a708d4a1e8
Aditya Jain - www.aadityajain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2023-05-11 23:51:13 +0530

    padding props to inner page component

--- src/components/innerPageComponent/innerPageComponent.vue
+++ src/components/innerPageComponent/innerPageComponent.vue
@@ -1,10 +1,26 @@
 <script setup>
 import breadcrumbsComponent from '../breadcrumbsComponent/breadcrumbsComponent.vue';
+
+const props = defineProps({
+    staticTitle: {
+        required: true,
+        type: String,
+        default: '',
+    },
+    breadCrumbsArray: {
+        required: true,
+        type: Array,
+    },
+});
+
 </script>
 
 <template>
     <main id="main">
-        <breadcrumbsComponent />
+        <breadcrumbsComponent
+            :staticTitle="props.staticTitle"
+            :breadCrumbsArray="props.breadCrumbsArray"
+        />
 
         <section class="inner-page">
             <div class="container">
