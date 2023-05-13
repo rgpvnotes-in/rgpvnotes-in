49e1c5089110fc908eee41cae7f40b43d7b0ccce
Aditya Jain - www.aadityajain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2023-05-13 11:26:17 +0530

    component for handeling page

---
+++ src/components/pageComponent/pageComponent.vue
@@ -0,0 +1,36 @@
+<script setup>
+import '@assets/css/blogger-style.css';
+import StaticTitleCard from '../postBodyComponent/blackBackground.vue';
+import MultiColorCard from '../postBodyComponent/multiColorBackground.vue';
+
+const props = defineProps({
+    getBranchYearDataArray: {
+        required: true,
+        type: Array,
+    },
+});
+
+const semesterArray = [];
+for (const subjectsData of props.getBranchYearDataArray) {
+    semesterArray.push(subjectsData.semester);
+}
+
+
+let filteredSemesterArray = [...new Set(semesterArray)].sort();
+
+const filteredSubjectsData = (semesterName) =>
+    props.getBranchYearDataArray
+        .filter((subjectsData) => subjectsData.semester === semesterName)
+        .sort();
+</script>
+
+<template>
+    <div v-for="semesterData in filteredSemesterArray" :key="semesterData">
+        <StaticTitleCard>
+            {{ semesterData }}
+        </StaticTitleCard>
+        <MultiColorCard
+            :subjectsDataList="filteredSubjectsData(semesterData)"
+        />
+    </div>
+</template>
