9d24fc335e3123523426e3226ae970eee3f27bc8
Aditya Jain - www.aadityajain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2023-05-13 11:25:46 +0530

    adding title & text content block component

---
+++ src/components/postBodyComponent/titleTextBlock.vue
@@ -0,0 +1,27 @@
+<script setup>
+import StaticTitleCard from './blackBackground.vue';
+
+const props = defineProps({
+    title: {
+        required: true,
+        type: String,
+    },
+    textArray: {
+        required: true,
+        type: Array,
+    },
+});
+
+</script>
+
+<template>
+    <div v-if="1">
+        <StaticTitleCard>
+        {{ props.title }}
+    </StaticTitleCard>
+    <p v-for="textData in props.textArray" :key="textData">
+        {{ textData }}
+    </p>
+    </div>
+
+</template>
