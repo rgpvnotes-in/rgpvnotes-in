e6c3b6eead79919a107789e59c132965d172fb84
Aditya Jain - www.aadityajain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2023-05-18 02:03:26 +0530

    adding sitemap generator

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import vue from "@astrojs/vue";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  site: 'https://www.rgpvnotes.in/',
+  integrations: [vue(), sitemap()]
 });
