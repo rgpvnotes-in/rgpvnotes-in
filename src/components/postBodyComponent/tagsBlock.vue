bb326b471929376e31bc2cd70d71d4dab543cdd7
Aditya Jain - www.aadityajain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2023-05-18 01:08:00 +0530

    reusing this component for side bar also

--- src/components/postBodyComponent/tagsBlock.vue
+++ src/components/postBodyComponent/tagsBlock.vue
@@ -4,14 +4,19 @@ const props = defineProps({
         required: true,
         type: Array,
     },
+    showTag: {
+        required: true,
+        type: Boolean,
+    },
 });
 </script>
 
 <template>
     <div class="d-flex align-items-center mb-3">
-        <strong class="me-2">TAGS: </strong>
+        <strong v-if="showTag" class="me-2">TAGS: </strong>
         <span
-            class="badge rounded-pill bg-primary py-2 px-3 me-2"
+            class="badge bg-primary py-2 px-3 me-2"
+            :class="[showTag ? 'rounded-pill' : '']"
             v-for="label in props.labelArray"
             :key="label"
             >{{ label }}</span
