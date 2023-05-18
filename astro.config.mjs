cbad046c789ec99d522a0d63463020a9b932f27b
Aditya Jain - www.aadityajain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2023-05-18 15:56:55 +0530

    removed old sitemap added filter for sitemap generation added changefrequency, last update & priority for sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 import { defineConfig } from 'astro/config';
-import vue from "@astrojs/vue";
+import vue from '@astrojs/vue';
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.rgpvnotes.in/',
-  integrations: [vue(), sitemap()]
+    site: 'https://www.rgpvnotes.in/',
+    integrations: [
+        vue(),
+        sitemap({
+            changefreq: 'weekly',
+            priority: 0.7,
+            lastmod: new Date(),
+            filter: (page) =>
+                page !== 'https://www.rgpvnotes.in/whatsapp/' &&
+                page !== 'https://www.rgpvnotes.in/not-available/',
+        }),
+    ],
 });
