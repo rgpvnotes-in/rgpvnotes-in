e073299d44b79129d4a635214ee94b1725f81b2e
Aditya Jain - www.aadityajain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2023-05-19 01:33:37 +0530

    adding top border to Tags component

--- src/components/postBodyComponent/tagsBlock.vue
+++ src/components/postBodyComponent/tagsBlock.vue
@@ -12,7 +12,7 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="d-flex align-items-center mb-3">
+    <div class="d-flex align-items-center my-3 border-dark border-1 border-top pt-2">
         <strong v-if="showTag" class="me-2">TAGS: </strong>
         <span
             class="badge bg-primary py-2 px-3 me-2"
