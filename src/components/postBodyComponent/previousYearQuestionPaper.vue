19ad4ecd34ae271a83b97c377295fa8d4923350d
Aditya Jain - www.aadityajain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2023-05-19 04:29:37 +0530

    questionPaperData is array, making changes for it

--- src/components/postBodyComponent/previousYearQuestionPaper.vue
+++ src/components/postBodyComponent/previousYearQuestionPaper.vue
@@ -1,6 +1,22 @@
 <script setup>
 import StaticTitleCard from './blackBackground.vue';
 
+const urlToName = (urlValue) => {
+    const splitUrl = urlValue.split('/');
+    const nameOnly = splitUrl[splitUrl.length - 1];
+    let tempName = nameOnly.split('-');
+    const subjectCode = tempName.slice(0, 2).join('-').toUpperCase();
+    tempName = tempName.slice(2);
+
+    //loop through each element of the array and capitalize the first letter.
+    for (var i = 0; i < tempName.length; i++) {
+        tempName[i] = tempName[i].charAt(0).toUpperCase() + tempName[i].slice(1);
+    }
+
+    const subjectName = tempName.join('-').replace('.pdf', '');
+    return `${subjectCode}-${subjectName}`;
+};
+
 const props = defineProps({
     questionPaperDataArray: {
         required: true,
@@ -11,13 +27,17 @@ const props = defineProps({
 </script>
 
 <template>
-    <div v-if="props.questionPaperDataArray.length > 0">
+    <div v-if="props.questionPaperDataArray.length > 0" id="questionPapers">
         <hr class="cool-line" />
         <StaticTitleCard> Previous Year Question Papers </StaticTitleCard>
         <ul class="list-group">
-            <li v-for="questionPaperData in props.questionPaperDataArray" :key="questionPaperData.post_id" class="list-group-item border-0 bg-transparent ps-0">
-                <a :href="questionPaperData.slug" class="text-dark fw-bold"
-                    > {{ questionPaperData.rendered_title }}</a
+            <li
+                v-for="questionPaperData in props.questionPaperDataArray"
+                :key="questionPaperData"
+                class="list-group-item border-0 bg-transparent ps-0"
+            >
+                <a :href="questionPaperData" class="text-dark fw-bold">
+                    {{ urlToName(questionPaperData) }}</a
                 >
             </li>
         </ul>
