9700a109414849635f60a47973c90dd0b8d81daa
Aditya Jain - www.aadityajain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2023-05-20 01:50:39 +0530

    added scoped css

--- src/components/postBodyComponent/blackBackground.vue
+++ src/components/postBodyComponent/blackBackground.vue
@@ -7,3 +7,29 @@
         </div>
     </div>
 </template>
+
+<style scoped>
+.post-body-title-card {
+    text-align: center;
+    background: #000;
+    border-radius: 2px;
+    display: inline-block;
+    padding: 10px;
+    margin: 1rem;
+    width: 90%;
+    color: #fff;
+    font-weight: 700;
+    font-family: inherit;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  }
+  
+  .post-body-title-card:hover {
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+  }
+  
+  .post-body-title-card h2 {
+    margin-bottom: 0;
+    font-size: 30px;
+  }
+  
+</style>
