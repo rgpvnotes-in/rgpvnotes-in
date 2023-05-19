5ae07883924fc17a47d80e294c77125e2e75bd93
Aditya Jain - www.aadityajain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2023-05-20 01:36:39 +0530

    removed unused css

--- src/components/homePage/heroComponent/heroComponent.vue
+++ src/components/homePage/heroComponent/heroComponent.vue
@@ -1,7 +1,6 @@
 <script setup>
 import RightFloatingImage from './partials/FloatingImage.vue';
 import LeftSearchAndSelector from './partials/SearchAndSelector.vue';
-
 </script>
 
 <template>
@@ -16,7 +15,6 @@ import LeftSearchAndSelector from './partials/SearchAndSelector.vue';
     </section>
     <!-- End Hero -->
 </template>
-
 
 <style scoped>
 /*--------------------------------------------------------------
@@ -34,45 +32,8 @@ import LeftSearchAndSelector from './partials/SearchAndSelector.vue';
     background-size: cover;
 }
 
-#hero .container,
 #hero .container-fluid {
     padding-top: 84px;
-}
-
-#hero h1 {
-    margin: 0;
-    font-size: 52px;
-    font-weight: 700;
-    line-height: 64px;
-    color: #fff;
-}
-
-#hero h2 {
-    color: rgba(255, 255, 255, 0.8);
-    margin: 10px 0 0 0;
-    font-size: 20px;
-}
-
-#hero .btn-get-started {
-    font-family: 'Poppins', sans-serif;
-    font-weight: 500;
-    font-size: 16px;
-    letter-spacing: 1px;
-    display: inline-block;
-    padding: 10px 28px;
-    border-radius: 5px;
-    transition: 0.5s;
-    color: #fff;
-    border: 2px solid #fff;
-}
-
-#hero .btn-get-started:hover {
-    background: #fff;
-    color: #5846f9;
-}
-
-#hero .animated {
-    animation: up-down 2s ease-in-out infinite alternate-reverse both;
 }
 
 @media (min-width: 1200px) {
@@ -86,59 +47,14 @@ import LeftSearchAndSelector from './partials/SearchAndSelector.vue';
         text-align: center;
     }
 
-    #hero .container,
     #hero .container-fluid {
         padding-top: 68px;
-    }
-
-    #hero .animated {
-        animation: none;
-    }
-
-    #hero .hero-img {
-        text-align: center;
-    }
-
-    #hero .hero-img img {
-        width: 50%;
-    }
-}
-
-@media (max-width: 768px) {
-    #hero h1 {
-        font-size: 26px;
-        line-height: 36px;
-    }
-
-    #hero h2 {
-        font-size: 18px;
-        line-height: 24px;
-    }
-
-    #hero .hero-img img {
-        width: 60%;
-    }
-}
-
-@media (max-width: 575px) {
-    #hero .hero-img img {
-        width: 80%;
     }
 }
 
 @media (max-width: 575px), (max-height: 600px) {
     #hero {
         height: auto;
-    }
-}
-
-@keyframes up-down {
-    0% {
-        transform: translateY(10px);
-    }
-
-    100% {
-        transform: translateY(-10px);
     }
 }
 </style>
--- src/components/homePage/heroComponent/partials/FloatingImage.vue
+++ src/components/homePage/heroComponent/partials/FloatingImage.vue
@@ -13,126 +13,25 @@ import heroImg from '../../../../assets/img/hero-img.png';
 </template>
 
 <style scoped>
-/*--------------------------------------------------------------
-# Hero Section
---------------------------------------------------------------*/
-#hero {
-    width: 100%;
-    height: 100vh;
-    background: linear-gradient(
-            45deg,
-            rgba(86, 58, 250, 0.9) 0%,
-            rgba(116, 15, 214, 0.9) 100%
-        ),
-        url('/assets/img/hero-bg.jpg') center center no-repeat;
-    background-size: cover;
-}
-
-#hero .container,
-#hero .container-fluid {
-    padding-top: 84px;
-}
-
-#hero h1 {
-    margin: 0;
-    font-size: 52px;
-    font-weight: 700;
-    line-height: 64px;
-    color: #fff;
-}
-
-#hero h2 {
-    color: rgba(255, 255, 255, 0.8);
-    margin: 10px 0 0 0;
-    font-size: 20px;
-}
-
-#hero .btn-get-started {
-    font-family: 'Poppins', sans-serif;
-    font-weight: 500;
-    font-size: 16px;
-    letter-spacing: 1px;
-    display: inline-block;
-    padding: 10px 28px;
-    border-radius: 5px;
-    transition: 0.5s;
-    color: #fff;
-    border: 2px solid #fff;
-}
-
-#hero .btn-get-started:hover {
-    background: #fff;
-    color: #5846f9;
-}
-
-#hero .animated {
-    animation: up-down 2s ease-in-out infinite alternate-reverse both;
-}
-
-@media (min-width: 1200px) {
-    #hero {
-        background-attachment: fixed;
-    }
-}
-
 @media (max-width: 991px) {
-    #hero {
+    .hero-img {
         text-align: center;
     }
 
-    #hero .container,
-    #hero .container-fluid {
-        padding-top: 68px;
-    }
-
-    #hero .animated {
-        animation: none;
-    }
-
-    #hero .hero-img {
-        text-align: center;
-    }
-
-    #hero .hero-img img {
+    .hero-img img {
         width: 50%;
     }
 }
 
 @media (max-width: 768px) {
-    #hero h1 {
-        font-size: 26px;
-        line-height: 36px;
-    }
-
-    #hero h2 {
-        font-size: 18px;
-        line-height: 24px;
-    }
-
-    #hero .hero-img img {
+    .hero-img img {
         width: 60%;
     }
 }
 
 @media (max-width: 575px) {
-    #hero .hero-img img {
+    .hero-img img {
         width: 80%;
-    }
-}
-
-@media (max-width: 575px), (max-height: 600px) {
-    #hero {
-        height: auto;
-    }
-}
-
-@keyframes up-down {
-    0% {
-        transform: translateY(10px);
-    }
-
-    100% {
-        transform: translateY(-10px);
     }
 }
 </style>
