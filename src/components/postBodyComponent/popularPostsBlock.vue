dfcaa86d1b5a75005ffc905d2823c49bd747972a
Aditya Jain - www.aadityajain.dev <61523512+AadityaJain-Dev@users.noreply.github.com>
2023-05-22 05:25:35 +0530

    removing padding from image

--- src/components/postBodyComponent/popularPostsBlock.vue
+++ src/components/postBodyComponent/popularPostsBlock.vue
@@ -11,7 +11,7 @@ const props = defineProps({
     <div class="d-flex align-items-center mb-3">
         <div>
             <a :href="post.slug" v-for="post in props.postsArray" :key="post.slug" class="row">
-            <div class="col-3"><img :src="post.banner_url" :alt="post.rendered_title" class="w-100 h-auto"></div>
+            <div class="col-3 px-0"><img :src="post.banner_url" :alt="post.rendered_title" class="w-100 h-auto"></div>
             <div class="col-9"> <h3 class="h6"> {{post.rendered_title}}</h3></div>
             </a>
         </div>
