f6c2298f19276e2afc158de929197dac3874a4d0
Aditya Jain <61523512+AadityaJain-Dev@users.noreply.github.com>
2023-12-10 20:47:47 +0530

    adding precommit hook, linting and sitemap to project

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:vue/vue3-essential',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+    ],
+    overrides: [],
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['vue', '@typescript-eslint'],
+    rules: {},
+};
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
 import { defineConfig } from 'astro/config';
 
 import vue from "@astrojs/vue";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.rgpvnotes.in/',
-  integrations: [vue()]
+    site: 'https://www.rgpvnotes.in/',
+    integrations: [
+        vue(),
+        sitemap({
+            changefreq: 'weekly',
+            priority: 0.7,
+            lastmod: new Date(),
+            filter: (page) =>
+                page !== 'https://www.rgpvnotes.in/whatsapp/' &&
+                page !== 'https://www.rgpvnotes.in/not-available/',
+        }),
+    ],
 });
