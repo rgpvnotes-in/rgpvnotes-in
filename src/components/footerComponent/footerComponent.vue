bfcd95aab521310003c7055c9503b847e8a2667e
Aditya Jain <61523512+AadityaJain-Dev@users.noreply.github.com>
2023-12-11 00:50:49 +0530

    disabling preloader for now and delete based slug template

--- src/components/footerComponent/footerComponent.vue
+++ src/components/footerComponent/footerComponent.vue
@@ -303,7 +303,7 @@ const props = defineProps({
       class="back-to-top d-flex align-items-center justify-content-center"
       ><i class="bi bi-arrow-up-short"></i
     ></a>
-    <div id="preloader"></div>
+    <!-- <div id="preloader"></div> -->
   </div>
 </template>
 
